290b8d49aef2fc7de569831e2a34e8c8f5c24e5f
Johannes Hoff <johshoff@gmail.com>
2015-07-06 20:42:10 -0700

    Implement point operators
    Add and Sub of other points,
    Mul and Div for scalars

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use std::fs::File;
 use std::io::Read;
 use std::fmt::{ Display, Formatter };
 use std::fmt;
+use std::ops::{ Sub, Add, Mul, Div };
+use std::clone::Clone;
 
 use gl::types::*;
 use glfw::{Context, Key, OpenGlProfileHint, Window, WindowHint, WindowMode};
@@ -38,6 +40,7 @@ mod mandel {
     }
 }
 
+#[derive(Copy, Clone)]
 struct Point<T> {
     x : T,
     y : T,
@@ -48,6 +51,47 @@ impl<T> Point<T> {
         Point { x: x, y: y }
     }
 }
+
+impl<T: Add<T, Output=T> + Clone> Add for Point<T> {
+    type Output = Point<T>;
+
+    fn add(self, rhs: Point<T>) -> Point<T> {
+        Point { x: self.x + rhs.x,
+                y: self.y + rhs.y }
+    }
+}
+
+impl<T: Sub<T, Output=T> + Clone> Sub for Point<T> {
+    type Output = Point<T>;
+
+    fn sub(self, rhs: Point<T>) -> Point<T> {
+        Point { x: self.x - rhs.x,
+                y: self.y - rhs.y }
+    }
+}
+
+/// Multiply each element by a constant
+impl<T: Mul<T, Output=T> + Copy> Mul<T> for Point<T> {
+    type Output = Point<T>;
+
+    fn mul(self, rhs: T) -> Point<T> {
+        Point { x: self.x * rhs,
+                y: self.y * rhs }
+    }
+}
+
+/// Divide each element by a constant
+impl<T: Div<T, Output=T> + Copy> Div<T> for Point<T> {
+    type Output = Point<T>;
+
+    fn div(self, rhs: T) -> Point<T> {
+        Point { x: self.x / rhs,
+                y: self.y / rhs }
+    }
+}
+
+
+
 
 struct Line {
     y: i32,
@@ -291,8 +335,7 @@ fn main() {
                 }
                 glfw::WindowEvent::MouseButton(glfw::MouseButton::Button1, glfw::Action::Press, _) => {
                     mouse_button_1_pressed = true;
-                    mouse_start_pan.x = mouse.x;
-                    mouse_start_pan.y = mouse.y;
+                    mouse_start_pan = mouse;
                 }
                 glfw::WindowEvent::MouseButton(glfw::MouseButton::Button1, glfw::Action::Release, _) => {
                     mouse_button_1_pressed = false;
@@ -304,10 +347,8 @@ fn main() {
                     if mouse_button_1_pressed {
                         let world_per_pixel = world_width_from_zoom(zoom) / (pixels.x as f64);
                         let world_per_point = world_per_pixel * (pixel_size as f64);
-                        center.x -= (mouse.x - mouse_start_pan.x) * world_per_point;
-                        center.y -= (mouse.y - mouse_start_pan.y) * world_per_point;
-                        mouse_start_pan.x = mouse.x;
-                        mouse_start_pan.y = mouse.y;
+                        center = center - (mouse - mouse_start_pan) * world_per_point;
+                        mouse_start_pan = mouse;
 
                         needs_redraw = true;
                     }
