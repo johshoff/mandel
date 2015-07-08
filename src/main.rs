5530e9e32d6e2d6406c7df3c1176c567e56b1e84
Johannes Hoff <johshoff@gmail.com>
2015-07-07 23:00:34 -0700

    Extract point into its own crate and repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate gl;
 extern crate glfw;
 extern crate time;
+extern crate point;
 
 use std::sync::mpsc::channel;
 use std::thread::spawn;
@@ -11,12 +12,11 @@ use std::fs::File;
 use std::io::Read;
 use std::fmt::{ Display, Formatter };
 use std::fmt;
-use std::ops::{ Sub, Add, Mul, Div };
-use std::clone::Clone;
 
 use gl::types::*;
 use glfw::{Context, Key, OpenGlProfileHint, Window, WindowHint, WindowMode};
 
+use point::Point;
 mod gl_util;
 
 mod mandel {
@@ -39,58 +39,6 @@ mod mandel {
         return DETAIL;
     }
 }
-
-#[derive(Copy, Clone)]
-struct Point<T> {
-    x : T,
-    y : T,
-}
-
-impl<T> Point<T> {
-    fn new(x: T, y: T) -> Point<T> {
-        Point { x: x, y: y }
-    }
-}
-
-impl<T: Add<T, Output=T> + Clone> Add for Point<T> {
-    type Output = Point<T>;
-
-    fn add(self, rhs: Point<T>) -> Point<T> {
-        Point { x: self.x + rhs.x,
-                y: self.y + rhs.y }
-    }
-}
-
-impl<T: Sub<T, Output=T> + Clone> Sub for Point<T> {
-    type Output = Point<T>;
-
-    fn sub(self, rhs: Point<T>) -> Point<T> {
-        Point { x: self.x - rhs.x,
-                y: self.y - rhs.y }
-    }
-}
-
-/// Multiply each element by a constant
-impl<T: Mul<T, Output=T> + Copy> Mul<T> for Point<T> {
-    type Output = Point<T>;
-
-    fn mul(self, rhs: T) -> Point<T> {
-        Point { x: self.x * rhs,
-                y: self.y * rhs }
-    }
-}
-
-/// Divide each element by a constant
-impl<T: Div<T, Output=T> + Copy> Div<T> for Point<T> {
-    type Output = Point<T>;
-
-    fn div(self, rhs: T) -> Point<T> {
-        Point { x: self.x / rhs,
-                y: self.y / rhs }
-    }
-}
-
-
 
 
 struct Line {
